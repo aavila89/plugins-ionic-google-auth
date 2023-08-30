97335b3f244c582d8e63d3d74e5b19481c1875cd
aavila89 <90415322+aavila89@users.noreply.github.com>
2023-08-29 21:36:05 -0400

    feat: Implemented google auth

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Device } from '@capacitor/device';
+import { AlertController } from '@ionic/angular';
+import {GoogleAuth, User} from '@codetrix-studio/capacitor-google-auth';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor() {}
+  user = {
+    email: '',
+    name: '',
+    givenName: '',
+    familyName: '',
+    imageUrl: ''
+  };
+  constructor(
+    private alertCtrl: AlertController
+  ) {}
+
+  async ngOnInit() {
+    const info = await Device.getInfo();
+    if (info.platform !== 'web') {
+      GoogleAuth.initialize();
+    }
+  }
+
+  async signIn() {
+    const {email, givenName, familyName, name, imageUrl}: User = await GoogleAuth.signIn();
+    this.user = {
+      email,
+      name,
+      givenName,
+      familyName,
+      imageUrl
+    };
+  }
+
+  async presentAlert(title?: string, message?: string) {
+    const alert = await this.alertCtrl.create({
+      header: title,
+      message,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
 
 }
