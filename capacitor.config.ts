c694a62388263595277ec3675c0922ee07eefd77
aavila89 <90415322+aavila89@users.noreply.github.com>
2023-08-29 21:35:20 -0400

    feat: Added serverClientId and scopes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,21 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.pushnotification.cap',
   appName: 'base',
   webDir: 'www',
   server: {
     androidScheme: 'https'
+  },
+  plugins: {
+    "GoogleAuth": {
+      "scopes": [
+        "profile",
+        "email"
+      ],
+      "serverClientId": "66853167924-97ad2ffpbg92rkob27o91qh6odffc29d.apps.googleusercontent.com",
+      "forceCodeForRefreshToken": true
+    }
   }
 };
 
